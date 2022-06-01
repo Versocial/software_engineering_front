401af338fd333ca57c85523ce5e7c227bf272dd4
hangcaihui <wangzhe177@bupt.edu.cn>
2022-06-01 19:01:07 +0800

    add usreResp

--- app/src/main/java/com/example/software_engineer/ui/user.kt
+++ app/src/main/java/com/example/software_engineer/ui/user.kt
@@ -3,4 +3,7 @@ package com.example.software_engineer.ui
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter=true)
-data class user(val username:String="",val password:String="")
+data class userParam(val username:String="", val password:String="")
+
+@JsonClass(generateAdapter=true)
+data class userResp(val user_id:Int=-1,val token:String="",val status_code:Int=-1,val status_msg:String="")
